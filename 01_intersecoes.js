b80aa1c64e8a59d83665887c2994c84e8abb01b7
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2023-02-10 16:43:30 -0300

    reviewing and commenting 'intersecoes'

--- 01_intersecoes.js
+++ 01_intersecoes.js
@@ -6,35 +6,11 @@ Entrada: ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
 Saída: 1,9,10
 */
 
-function stringToList(inputString) {
-  /*
-    Admitindo que na string de entrada desta funcao
-    cada numero esteja separado por "," (virgula)
-    ou ", " (virgula seguida de espaco),
-    a funcao string.split() eh usada para transformar
-    a string de entrada num array em que cada elemento
-    eh uma 'string numerica'.
-
-    O array contendo as 'strings numericas' eh retornado
-    diretamente por esta funcao:
-  */
-  const arrayOfStrings = inputString.split(",");
-  return arrayOfStrings;
-}
-
 function stringsToNumbers(inputArray) {
-  /*
-    variavel que contera o array de numeros obtidos
-    a partir do array de 'strings numericas':
-  */
-  const arrayOfNumbers = [];
+  // variavel contera o array de numeros obtidos a partir do array de entrada
+  const arrayOfNumbers = []; 
   
-  /*
-  optei por nao usar higher order functions,
-  mas poderia ser usado p.ex. map para converter
-  cada elemento do array de strings numericas, no lugar
-  do loop for puro
-  */
+  // convertendo cada string numerica do array de entrada para seu valor numerico na base 10, e armazenando no array final que sera retornado:
   for (let i = 0; i < inputArray.length; i++) {
     arrayOfNumbers.push(parseInt(inputArray[i]));
   }
@@ -42,77 +18,60 @@ function stringsToNumbers(inputArray) {
   return arrayOfNumbers;
 }
 
-function searchRepeated(firstArray, secondArray) {
-  const hashTable = {};
-  const outputArray = [];
+function searchRepeated(stringList) {
+  /*
+    Admitindo que nenhum elemento do array de entrada seja uma string vazia,
+    e que nos elementos do array cada numero esteja separado por "," (virgula) 
+    ou ", " (virgula seguida de espaco), a funcao string.split() eh
+    usada para transformar a string do elemento num array de 'strings numericas'.
+  */
+  const numericStrings0 = stringList[0].split(",");
+  const numericStrings1 = stringList[1].split(",");
 
-  // numeros ja sao ordenados (enunciado), facilita usar hash
-
-  // hash tbm eh usado pq funciona mesmo que tamanhos dos arrays seja diferente
+  // Convertendo arrays de strings em arrays de numeros, com a funcao declarada mais acima no codigo:
+  const numbersList0 = stringsToNumbers(numericStrings0);
+  const numbersList1 = stringsToNumbers(numericStrings1);
 
   /*
-    hash melhor tbm pois usando no lugar de p.ex. for's
-    aninhados, reduz complex. tempo de O(n^2) para
-    O(2n) = O(n). caso entrada fosse muito grande a
-    diferenca seria notada logo
+    Com os arrays de numeros obtidos optei por usar uma tabela hash simples
+    para verificar os numeros que constassem em ambos os arrays.
+    A tabela hash facilita obter a intersecao mesmo que originalmente os numeros
+    nao viessem ordenados como foi dito no enunciado.
   */
 
-  for (let i = 0; i < firstArray.length; i++) {
-    let element1 = firstArray[i];
-    if (!hashTable[element1]) {
-      hashTable[element1] = true;
+  const hashTable = {}; // tabela hash contera todos os numeros do primeiro array como chave, e estas chaves terao valor booleano 'true'
+  
+  const outputArray = []; // array contera o resultado final (intersecao dos numeros)
+  
+  // populando a hash table com todos os numeros do primeiro array:
+  for (let i = 0; i < numbersList0.length; i++) {
+    let actualNumber = numbersList0[i]; // numero 'atual' do array, visto na iteracao
+    if (!hashTable[actualNumber]) {
+      hashTable[actualNumber] = true; //adiciona o numero na tabela caso ainda nao esteja
     }
-    /*
-      tabela hash contera todos os numeros do 1o array
-      como chave, e valor 'true'
-    */
   }
 
-  for (let j = 0; j < secondArray.length; j++) {
-    let element2 = secondArray[j];
-    if (hashTable[element2]) {
-      outputArray.push(element2);
-    }
-    else {
-      continue;
-    }
-    /*
-      procura se na tabela hash o elemento atual
-      do segundo array ja existe (chave sera o numero,
-      valor true), se existe entao o numero eh repetido
-      e pode ser adicionado no array final
-    */
+  /*
+    percorre segundo array, procura na tabela hash se o numero 'atual'
+    na iteracao ja existe. Se existe entao o numero eh repetido e pode ser
+    adicionado ao array que contem o resultado final
+  */
+  for (let j = 0; j < numbersList1.length; j++) {
+    let actualNumber = numbersList1[j];
+    if (hashTable[actualNumber]) {  //verifica se numero atual ja esta na tabela
+      outputArray.push(actualNumber); //numero repetido vai para array de saida
+    }    
   }
 
   return outputArray;
 }
 
 
+const inputA = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"];
+console.log("Saida A: ", searchRepeated(inputA));
 
+const inputB = ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"];
+console.log("Saida B: ", searchRepeated(inputB));
 
-
-const input = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"];
-console.log("entrada0 pura: ", input[0]);
-console.log("entrada1 pura: ", input[1]);
-
-const arrayStringsNumeros0 = stringToList(input[0]);
-const arrayStringsNumeros1 = stringToList(input[1]);
-console.log("saida array strings0: ", arrayStringsNumeros0);
-console.log("saida array strings1: ", arrayStringsNumeros1);
-
-const arrayNumeros0 = stringsToNumbers(arrayStringsNumeros0);
-const arrayNumeros1 = stringsToNumbers(arrayStringsNumeros1);
-console.log("saida array numeros0: ", arrayNumeros0);
-console.log("saida array numeros1: ", arrayNumeros1);
-
-const arrayRepetidos = searchRepeated(arrayNumeros0, arrayNumeros1);
-console.log("resultado final repetidos: ", arrayRepetidos);
-
-
-// inputB = ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"];
-const inputA = [1, 3, 9, 10, 17, 18];
-const inputB = [1, 4, 9, 10];
-console.log("tamanhos diferentes: ");
-console.log("A: ", inputA);
-console.log("B: ", inputB);
-console.log("resultado tam dif: ", searchRepeated(inputA, inputB));
+const inputC = ["-3, -2, 0, 5, 8, 11", "-3, -1, 0, 3, 4, 5, 6, 7, 9, 11, 15"];
+console.log("Saida C: ", searchRepeated(inputC));
